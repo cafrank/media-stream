a8d3491993a20a2fd57c56df037d3a3e396c195d
Colin Frank <cfrank@osafo.com>
2023-12-20 20:45:26 -0800

    MOving Keycloak service to GKE

--- keycloak-provider/build.sh
+++ keycloak-provider/build.sh
@@ -1,11 +1,24 @@
+#==================================================
 # microk8s enable registry:size=40Gi
 # docker build --build-arg JAR_FILE=target/\*.jar -t adapter .  && \
+#==================================================
+#mvn clean package -DskipTests=true   && \
+#docker build -t kclatest .  && \
+#docker tag kclatest localhost:32000/kclatest   && \
+#docker push localhost:32000/kclatest   && \
+#microk8s ctr image pull --plain-http localhost:32000/kclatest:latest && \
+#kubectl -n djmz delete -f k8s-keycloak.yaml && \
+#kubectl -n djmz apply  -f k8s-keycloak.yaml && \
+#kubectl -n djmz get pod
+
+
+#==================================================
+# hub.docker.com: docker login --username=cafrank
+#==================================================
 mvn clean package -DskipTests=true   && \
 docker build -t kclatest .  && \
-docker tag kclatest localhost:32000/kclatest   && \
-docker push localhost:32000/kclatest   && \
-microk8s ctr image pull --plain-http localhost:32000/kclatest:latest && \
+docker tag kclatest cafrank/kclatest
+docker push cafrank/kclatest
 kubectl -n djmz delete -f k8s-keycloak.yaml && \
 kubectl -n djmz apply  -f k8s-keycloak.yaml && \
 kubectl -n djmz get pod
-
